2d9ad0cbade0332c65568ea00839af8afd6a49dd
Alan Ayalae <alay7359@colorado.edu>
2025-08-16 14:11:57 -0600

    Added Analyzer dB Marks / Clean-Up

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -349,6 +349,7 @@ void ResponseCurveComponent::resized()
     auto width = renderArea.getWidth();
     
     Array<float> xs;
+    
     for( auto f : freqs )
     {
         auto normX = mapFromLog10(f, 20.f, 20000.f);
@@ -356,12 +357,9 @@ void ResponseCurveComponent::resized()
     }
     
     g.setColour(Colours::dimgrey);
-    //for( auto f : freqs )
+    
     for ( auto x : xs )
     {
-        //auto normX = mapFromLog10(f, 20.f, 20000.f);
-        
-        //g.drawVerticalLine(getWidth() * normX, 0.f, getHeight());
         g.drawVerticalLine(x, top, bottom);
     }
     
@@ -373,12 +371,9 @@ void ResponseCurveComponent::resized()
     for( auto gDb : gain)
     {
         auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top) );
-        //g.drawHorizontalLine(y, 0, getWidth());
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::darkgrey );
         g.drawHorizontalLine(y, left, right);
     }
-    
-    //g.drawRect(getAnalysisArea());
     
     g.setColour(Colours::lightgrey);
     const int fontHeight = 10;
@@ -436,16 +431,21 @@ void ResponseCurveComponent::resized()
         
         g.drawFittedText(str, r, juce::Justification::centred, 1);
         
+        str.clear();
+        str << (gDb - 24.f);
+        
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+        
     }
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
 {
     auto bounds = getLocalBounds();
-    
-//    bounds.reduce(10, /*JUCE_LIVE_CONSTANT(5)*/
-//                  8 /*JUCE_LIVE_CONSTANT(5)*/
-//                  );
     
     bounds.removeFromTop(12);
     bounds.removeFromBottom(2);
