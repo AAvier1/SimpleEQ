c7de362637113b2f42b3ba83480c3ce0bfb66a37
Alan Ayalae <alay7359@colorado.edu>
2025-08-19 13:16:19 -0600

    Analyzer Button Path is created only once

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -246,6 +246,32 @@ private:
 };
 
 //==============================================================================
+
+struct PowerButton : juce::ToggleButton { };
+struct AnalyzerButton : juce::ToggleButton
+{
+    void resized() override
+    {
+        auto bounds = getLocalBounds();
+        auto insetRect = bounds.reduced(4);
+        
+        randomPath.clear();
+        
+        juce::Random r;
+        
+        randomPath.startNewSubPath(insetRect.getX(),
+                                   insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+        
+        for( auto x = insetRect.getX() + 1; x < insetRect.getRight(); x += 2 )
+        {
+            randomPath.lineTo(x, insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+        }
+        
+    }
+    
+    juce::Path randomPath;
+};
+
 /**
 */
 class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
@@ -284,7 +310,8 @@ private:
                 lowCutSlopeSliderAttachment,
                 highCutSlopeSliderAttachment;
     
-    juce::ToggleButton lowcutBypassButton, peakBypassButton, highcutBypassButton, analyzerEnabledButton;
+    PowerButton lowcutBypassButton, peakBypassButton, highcutBypassButton;
+    AnalyzerButton analyzerEnabledButton;
     
     using ButtonAttatchment = APVTS::ButtonAttachment;
     ButtonAttatchment lowcutBypassButtonAttachment,
